54cf0805d4bd05b5459de355d62e52e85326fb8c
Go-gray <109012401+Go-gray@users.noreply.github.com>
2022-07-31 23:34:12 +0900

    4week

---
+++ App.js
@@ -0,0 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import ProductDetail from "./pages/ProductDetail";
+import Basket from "./pages/Basket";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/product/:productId" element={<ProductDetail />} />
+        <Route path="/basket" element={<Basket />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
+export default App;
